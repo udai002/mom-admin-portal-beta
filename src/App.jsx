2279a8e534a8d7a857df8a53f629756a22dce1cc
Paluchani Shiva Kumar Reddy <shiva@Paluchanis-MacBook-Air.local>
2025-06-10 10:43:14 +0530

    added more

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,17 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 import { Route, Routes } from 'react-router'
-import Home from './pages/Home'
 import Orders from './pages/Orders/orders'
+import Feedback from './pages/Feedback/UserFeedback'
+import {Dashboard} from './pages/DashBoard/Dashboard'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <Routes>
-      <Route path='/' Component={Orders} />
-    </Routes>
+      <Route path='/dashboard' Component={Dashboard} />
+      <Route path='/orders' Component={Orders} />
+      <Route path='/feedback' Component={Feedback} />
+      </Routes>
   )
 }
 
--- src/pages/Orders/Orders.jsx
+++ src/pages/Orders/Orders.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import TopBar from '../../components/Orders/topbar'
 import TotalOrders from '../../components/Orders/TotalOrders'
+import OrderTable from '../../components/Orders/OrderTable'
 
 function Orders() {
   return (
     <>
     <TopBar />
     <TotalOrders />
+    <OrderTable />
     </>
   )
 }
